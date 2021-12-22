9191ad4b5c65d67581f72ff617415a07081cc1f6
Vasmayer <lealimarco90@gmail.com>
2021-12-22 14:47:58 +0100

    added human code analysis

--- js/script.js
+++ js/script.js
@@ -1,3 +1,17 @@
 /* Scrivi un programma che stampi i numeri da 1 a 100,
 ma per i multipli di 3 stampi Fizz al posto del numero e per i multipli di 5 stampi Buzz.
-Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz. */
+Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz. 
+
+BONUS:
+Provare a stampare la lista in pagina invece che in console.
+
+1. creo un ciclo che vada da 1 a 100 compreso
+2. all'interno del ciclo verifico quali numeri sono divisibili per 3 e per 5
+3.per i numeri divisibili per 3 stampo Fizz per 5 Bazz entrambi FizzBazz
+
+*/
+
+for(let i=1;i>=numberOfCycles)
+{
+
+}
